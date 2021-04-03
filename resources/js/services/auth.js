00c0aa0e258e4b242540638122b36b62652c150a
yurdmitriev <yurdmitriev02@gmail.com>
2021-04-04 01:11:31 +0300

    Auth service for SPA functionality

---
+++ resources/js/services/auth.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const authClient = axios.create({
+    // baseURL: 'https://messenger-course.herokuapp.com',
+    baseURL: process.env.VUE_APP_API_URL,
+    withCredentials: true
+});
+
+export default {
+    async login(payload) {
+        await authClient.get("/sanctum/csrf-cookie");
+        await authClient.post("/login", payload);
+    },
+    async registerUser(payload) {
+        await authClient.get("/sanctum/csrf-cookie");
+        await authClient.post("/register", payload);
+    },
+}
