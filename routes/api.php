6f30365b06eca0e50657a56d8d58bf63d87dc887
yurdmitriev <yurdmitriev02@gmail.com>
2021-04-05 11:35:26 +0300

    api: return info about logged in user

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+
     Route::get('/users/{id}', function ($id) {
         return User::findOrFail($id);
     });
