ccbaa68be0f3ae781ec9b75007226a6fd2fca89a
InLoveWithHer <vadim.savkunov7@gmail.com>
2021-04-08 00:10:03 +0300

    Create and display chats

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chat;
+use App\Models\Participant;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function index()
+    {
+        $user = User::find(Auth::id());
+        return $user->chats;
+    }
+
+    public function store(Request $request)
+    {
+        return Chat::create([
+            "about" => $request->about,
+            "user_id" => Auth::id(),
+        ]);
+    }
+
+
+    public function delete()
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,9 +21,14 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user/chats', [ChatController::class, 'index']);
+
     Route::get('/user', [UserController::class, 'index']);
 
     Route::get('/users/{id}', function ($id) {
         return User::findOrFail($id);
     });
+
+    Route::post('/chats/create', [ChatController::class, 'store']);
+
 });
