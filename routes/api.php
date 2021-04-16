938b4aecdb56ee93366589f44e5a42d8c340ea1c
yurdmitriev <yurdmitriev02@gmail.com>
2021-04-16 15:26:42 +0300

    api: require auth with sanctum for private broadcast channels

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ParticipantController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +22,9 @@ use Illuminate\Support\Facades\Route;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+
+
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
 
 Route::middleware(['auth:sanctum'])->group(function () {
