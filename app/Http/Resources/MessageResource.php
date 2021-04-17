b36a094fe3e9ba2e1589c46a0fec481d7cc17969
yurdmitriev <yurdmitriev02@gmail.com>
2021-04-17 09:27:25 +0300

    Basic resource classes for user and message

---
+++ app/Http/Resources/MessageResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MessageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'text' => $this->text,
+            'sender' => new UserBasicResource(User::findOrFail($this->user_id)),
+        ];
+    }
+}
