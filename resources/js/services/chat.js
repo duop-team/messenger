cf2bac353c83feb8b43fe4ac96c3d680a6987eb3
yurdmitriev <yurdmitriev02@gmail.com>
2021-04-17 14:05:58 +0300

    Frontend chat functionality service

--- resources/js/services/chat.js
+++ resources/js/services/chat.js
@@ -4,5 +4,17 @@ export default {
     },
     async listMessages(chat) {
         return await axios.get(`/api/chats/${chat}/messages`);
+    },
+    async listChats() {
+        return await axios.get('/api/user/chats');
+    },
+    async findUser(payload) {
+        return await axios.post('/api/users/search', payload);
+    },
+    async addParticipant(chat, payload) {
+        return await axios.post(`/api/chats/${chat}/participants/create`, payload)
+    },
+    async createChat(payload) {
+        return await axios.post('/api/chats/create', payload);
     }
 }
