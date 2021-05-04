295b8b910da722434b94d7fd05837520e32c6ea2
yurdmitriev <yurdmitriev02@gmail.com>
2021-05-05 00:21:40 +0300

    check if user a participant of chat

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -33,7 +33,7 @@ class Chat extends Model
         return $this->hasMany(Participant::class);
     }
 
-    public function hasParticipant($user): bool
+    public function hasParticipant($user):bool
     {
         return !!$this->participants()->where('user_id', $user->id);
     }
