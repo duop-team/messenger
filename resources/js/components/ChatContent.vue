e914109a726e5c4cc95a99f20fb8630dc45efda5
yurdmitriev <yurdmitriev02@gmail.com>
2021-05-17 00:30:58 +0300

    chat content component

--- resources/js/components/ChatContent.vue
+++ resources/js/components/ChatContent.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="chat__content">
-
+        <chat-header></chat-header>
+        <messages></messages>
+        <message-form></message-form>
     </div>
 </template>
 
 <script>
 export default {
-name: "ChatContent"
+    name: "ChatContent"
 }
 </script>
 
 <style scoped lang="scss">
-
+.chat__content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-between;
+}
 </style>
