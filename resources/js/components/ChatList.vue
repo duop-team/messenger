f01e5e9bed7485a4c770b990a753d9977bf4c598
yurdmitriev <yurdmitriev02@gmail.com>
2021-05-16 21:22:08 +0300

    using wrapper component for list of chats

---
+++ resources/js/components/ChatList.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul>
+        <li>
+            <chat-item v-for="chat in source" :key="chat.id"
+                       :class="['chat__item', ($store.getters['chats/currentChat'].id === chat.id) ? 'is-active' : '']"
+                       :title="chat.title"
+                       @click=""></chat-item>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "ChatList",
+    props: {
+        'source': {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+</style>
