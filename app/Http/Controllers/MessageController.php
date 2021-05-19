d6156f19d747d8e104c99265f8c33728cc4bc9fa
larkes00 <zakirov2802@gmail.com>
2021-05-19 09:35:32 +0300

    Moving validation to the MessageRequest class

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,16 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Events\MessageSent;
+use App\Http\Requests\Message\MessageRequest;
 use App\Http\Resources\MessageResource;
 use App\Models\Message;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 
 class MessageController extends Controller
 {
-    public function store(Request $request, $chat_id)
+    public function store(MessageRequest $request, $chat_id)
     {
 //        TODO: check if sender is participant
         $message = new MessageResource(Auth::user()->messages()->create([
@@ -35,16 +34,8 @@ class MessageController extends Controller
         return Message::findOrFail($message_id)->delete();
     }
 
-    public function update(Request $request, $message_id)
+    public function update(MessageRequest $request, $message_id)
     {
-        $validator = Validator::make($request->all(), [
-            'text' => ['required', 'string', 'min:1']
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(["message" => $validator->errors()->all()], 422);
-        }
-
         return Message::findOrFail($message_id)->update([
             'text' => $request->text
         ]);
