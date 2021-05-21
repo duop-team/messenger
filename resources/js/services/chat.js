1e52bb10b782a44c5bb5c7710c1eff159c8350c2
yurdmitriev <yurdmitriev02@gmail.com>
2021-05-21 22:56:02 +0300

    using new api routes in frontend services

--- resources/js/services/chat.js
+++ resources/js/services/chat.js
@@ -1,20 +1,20 @@
 export default {
     async sendMessage(chat, payload) {
-        return await axios.post(`/api/chats/${chat}/messages/send`, payload);
+        return await axios.post(`/api/chat/${chat}/messages`, payload);
     },
     async listMessages(chat) {
-        return await axios.get(`/api/chats/${chat}/messages`);
+        return await axios.get(`/api/chat/${chat}/messages`);
     },
     async listChats() {
         return await axios.get('/api/user/chats');
     },
     async findUser(payload) {
-        return await axios.post('/api/users/search', payload);
+        return await axios.post('/api/user/search', payload);
     },
     async addParticipant(chat, payload) {
-        return await axios.post(`/api/chats/${chat}/participants/create`, payload)
+        return await axios.post(`/api/chat/${chat}/participants`, payload)
     },
     async createChat(payload) {
-        return await axios.post('/api/chats/create', payload);
+        return await axios.post('/api/chats', payload);
     }
 }
