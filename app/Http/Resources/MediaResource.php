d047049d37b54d8b742f03491f3c97d04a0f4675
larkes00 <zakirov2802@gmail.com>
2021-05-30 12:43:47 +0300

    Created media resource for media controller

---
+++ app/Http/Resources/MediaResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class MediaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'content' => Storage::url($this->content),
+        ];
+    }
+}
