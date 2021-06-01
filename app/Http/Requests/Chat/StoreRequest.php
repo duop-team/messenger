19ef1a3a20d47212770c0e5d9892d73777275f75
larkes00 <zakirov2802@gmail.com>
2021-06-02 01:04:20 +0300

    Fixed validator for creation chat

--- app/Http/Requests/Chat/StoreRequest.php
+++ app/Http/Requests/Chat/StoreRequest.php
@@ -25,7 +25,7 @@ class StoreRequest extends FormRequest
     {
         return [
             'title' => ['required', 'min:1', 'max:255'],
-            'about' => ['required', 'min:1']
+            'about' => ['min:1']
         ];
     }
 }
