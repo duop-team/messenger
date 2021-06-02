a615ba4b5a9de022b472d77790ef8b804f795339
yurdmitriev <yurdmitriev02@gmail.com>
2021-06-03 00:10:11 +0300

    id field for chat resource

--- app/Http/Resources/ChatResource.php
+++ app/Http/Resources/ChatResource.php
@@ -16,6 +16,7 @@ class ChatResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'about' => $this->about,
             'photo' => new MediaResource(Media::where('id', $this->media_id)->first()),
