19523d084faaa175d07beda9e5bd60fee04af448
yurdmitriev <yurdmitriev02@gmail.com>
2021-06-03 21:49:52 +0300

    Add many participants to chat by nickname

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -17,14 +17,21 @@ class ParticipantController extends Controller
 
     public function store(Request $request, $chat_id)
     {
-        $participant = Participant::where('user_id', $request->user_id)->where('chat_id', $chat_id)->first();
-        if (!$participant) {
-            return Participant::create([
-                'chat_id' => $chat_id,
-                'user_id' => $request->user_id
-            ]);
+        if (empty($request->users)) {
+            return response()->noContent(404);
         }
-        return response()->noContent(404);
+        foreach ($request->users as $user) {
+            $temp = User::where('nickname', $user)->first();
+            $participant = Participant::where('user_id', $temp->id)->where('chat_id', $chat_id)->first();
+            if (!$participant) {
+                Participant::create([
+                    'chat_id' => $chat_id,
+                    'user_id' => $temp->id
+                ]);
+            }
+        }
+
+        return response()->noContent(200);
     }
 
     public function destroy($chat_id, $user_id)
