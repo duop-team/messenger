3e75aab77324076617333974f75606231ee7a438
yurdmitriev <yurdmitriev02@gmail.com>
2021-06-04 02:42:19 +0300

    Displaying full name of user

--- app/Http/Resources/UserBasicResource.php
+++ app/Http/Resources/UserBasicResource.php
@@ -16,6 +16,7 @@ class UserBasicResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'full_name' => $this->name,
             'nickname' => $this->nickname,
             'phone' => $this->phone,
             'photo' => new MediaResource(User::findOrFail($this->id)->media),
